c11381de001cb40d822fc281394ca3c258251ee1
Chris Keene <chriskeene@gmail.com>
2023-07-21 14:06:41 +0100

    env file load added

--- envstats/__init__.py
+++ envstats/__init__.py
@@ -1,6 +1,7 @@
 import os
 
 from flask import Flask
+from dotenv import load_dotenv
 
 
 def create_app(test_config=None):
@@ -10,6 +11,8 @@ def create_app(test_config=None):
         SECRET_KEY='dev',
         DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
     )
+
+    load_dotenv()  # take environment variables from .env.
 
     if test_config is None:
         # load the instance config, if it exists, when not testing
