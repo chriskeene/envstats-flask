dee201edc1e614be3128fbbf9f4e2f8033d9a78c
Chris Keene <chriskeene@gmail.com>
2023-08-15 10:11:00 +0100

    historic solar data now gets last 30 days

--- envstats/stats.py
+++ envstats/stats.py
@@ -73,9 +73,6 @@ def solarstats():
 def add_historic():
     pvl = PVLive()
 
-
-
-
     def daterange(start_date, end_date):
         for n in range(int((end_date - start_date).days)):
             yield start_date + timedelta(n)
@@ -101,6 +98,7 @@ def add_historic():
             time.sleep(1)
         else:
             print("already have: ", existingdata[0] )
+ 
             
 
 
