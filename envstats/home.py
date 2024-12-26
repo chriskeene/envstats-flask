720855cdf2088dbe803a3eaf24d509a7504c1046
Chris Keene <chriskeene@gmail.com>
2024-12-26 19:18:29 +0000

    minor changes to home

--- envstats/home.py
+++ envstats/home.py
@@ -6,10 +6,10 @@ home = Blueprint("home", __name__, url_prefix="/")
 @home.route("/home2")
 def home2():
     #return render_template("home.html")
-     return "<p>Hello, World!</p>"
+     return "<p>Hello, add /stats/ to the url</p>"
 
 @home.route('/')
 def home1():
-    return "<p>Hello, World!</p>"
+    return "<p>Hello, add /stats/ to the url</p>"
     #return render_template("home.html")
 
