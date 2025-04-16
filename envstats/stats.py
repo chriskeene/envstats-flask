108653f39bc524b650e2ccface3147ff413d4d59
Chris Keene <chriskeene@gmail.com>
2025-04-16 10:36:38 +0100

    make images linkable to full image, and slight chart tweaks

--- envstats/stats.py
+++ envstats/stats.py
@@ -60,12 +60,12 @@ def get_solar_data():
 
 def create_solar_chart1(df):
     df = df[['date','solartotal']]
-    ax = df.plot(kind='line', grid=1, fontsize=10, x="date")
+    ax = df.plot(kind='line', grid=1, fontsize=10, x="date", label="Solar")
+    plt.legend()
     plt.style.use('ggplot') 
     plt.xlabel("Month",  size = 10)
-    plt.legend(fontsize="8", loc ="lower left")
-    plt.ylabel("solar energy output", size = 10)
-    plt.title("UK Solar output since 2021", size = 15)
+    plt.ylabel("Solar energy output", size = 10)
+    plt.title("UK Solar output since 2021")
     plt.ticklabel_format(style='plain', axis='y')
     plt.savefig('envstats/static/images/solar1.png')
     plt.close() 
@@ -74,14 +74,14 @@ def create_solar_chart2(df):
     df4 = split_years(df)
     plt.style.use('ggplot') 
     plt.xlabel('Month', size = 10)
-    plt.ylabel('solar output')
+    plt.ylabel('Solar output', size = 10)
     for y in df4:
         y.name = y['year'].iloc[[0]].to_string(index=False)
         plt.plot(y['solartotal'].tolist(),label= y.name)
     plt.legend(loc='best')
     plt.title('Solar output per year')
     plt.ticklabel_format(style='plain', axis='y')
-    plt.xticks([0,1,2,3,4,5,6,7,8,9,10,11], ['Jan', 'Feb', 'Mar', 'Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'],rotation=20)
+    plt.xticks([0,1,2,3,4,5,6,7,8,9,10,11], ['Jan', 'Feb', 'Mar', 'Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'])
     plt.savefig('envstats/static/images/solar2.png')
     plt.close() 
 
@@ -113,7 +113,7 @@ def create_solar_table1(df):
 @stats.route("/solar")
 def solarstats():
     output = {
-        "title": "UK PV Solar every output",
+        "title": "UK Solar generation",
     }
     df = get_solar_data()
     # print table 
@@ -129,6 +129,26 @@ def solarstats():
     output["latest"] = query_db(latest_query)
     return render_template("stats/solar.html", output=output)
 
+@stats.route("/solar2")
+def solarstats2():
+    output = {
+        "title": "UK PV Solar every output2",
+    }
+    df = get_solar_data()
+    # print table 
+    df5 = create_solar_table1(df)
+    create_solar_chart1(df)
+    create_solar_chart2(df)
+    # the following takes df5 and converts it to html, most of the code is for converting to floats so that we can add thousand seperators
+    output["table1"] = [df5.astype('float64',errors='ignore').to_html(float_format=lambda x: format(x,',.0f'),classes='table table-sm table-hover data')]
+    output["table1json"] = df5.to_json(orient='records')
+    # Convert the DataFrame to a dictionary for Chart.js
+    data_dict = df5.to_dict(orient='list')
+    output["tjson2"] = jsonify(data_dict)
+    # the most recent date collected
+    latest_query = "SELECT date FROM public.solar ORDER BY date DESC limit 1"
+    output["latest"] = query_db(latest_query)
+    return render_template("stats/solar.html", output=output)
 
 # for running from command line. only retrieves solar data from api
 # for those days with no data. by default check the last 30 days
